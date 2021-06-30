0659a724b377cfc704d8dcd2bb9d378ddbe6837a
frankheyz <frankheyz@126.com>
2021-06-30 19:34:11 +0800

    add log_analysis.py

--- experiments/log_analysis.py
+++ experiments/log_analysis.py
@@ -1,6 +1,8 @@
 import os
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def read_log(log_file):
     # read txt
@@ -28,10 +30,9 @@ def read_log(log_file):
 
     f.close()
 
-    return image_size, ssim, psnr
+    return image_size, float(ssim), float(psnr)
 
 if __name__ == '__main__':
-    # read_log('/home/heyz/code/z-vision/results/20210630_01_32_35/log.txt')
     results_dir = '/home/heyz/code/z-vision/results'
     results_dirs = os.listdir(results_dir)
     results_dirs.sort()
@@ -54,6 +55,11 @@ if __name__ == '__main__':
     ssim_list_sort = [ssim_list[i] for i in data_size_sort_index]
     psnr_list_sort = [psnr_list[i] for i in data_size_sort_index]
 
-    plt.plot(data_size_list, ssim_list_sort, 'ro')
+    plt.plot(data_size_list, psnr_list_sort, 'ro')
     plt.show()
-    pass
+
+    with open("datasize_psnr_ssim.csv", 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ')
+        writer.writerow(data_size_list)
+        writer.writerow(psnr_list_sort)
+        writer.writerow(ssim_list_sort)
