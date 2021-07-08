d4a4d742911276ea984000dba0c8ab3207371463
frankheyz <frankheyz@126.com>
2021-07-08 15:12:57 +0800

    add inference script

--- experiments/model_inference.py
+++ experiments/model_inference.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 import torch
 from zvision import ZVision
@@ -45,20 +46,51 @@ def infer_image(conf, image_path, model_path, model='old', times=2):
     return out
 
 
-if __name__ == "__main__":
-    old_model_path = '../results/20210629_20_49_43/checkpoint/model.pt'
-    upgraded_model_path = '../results/20210629_20_49_43/checkpoint/model.pt'
+def test_different_models(model_list, test_image, config):
+    for model in model_list:
+        infer_image(
+            conf=config,
+            image_path=test_image,
+            model_path=model,
+            model='new'
+        )
 
-    models = [
-        '../results/20210629_20_49_43/checkpoint/model.pt',
-        '../results/20210629_21_00_11/checkpoint/model.pt',
-        '../results/20210629_21_55_37/checkpoint/model.pt',
-    ]
-    for m in models:
+
+def test_different_images(model, image_list, config):
+    for img in image_list:
+        infer_image(
+            conf=config,
+            image_path=img,
+            model_path=model,
+            model='new'
+        )
+
+if __name__ == "__main__":
+    # old_modelodel_p_path = '../results/20210629_20_49_43/checkpoint/model.pt'
+    # upgraded_math = '../results/20210629_20_49_43/checkpoint/model.pt'
+
+    # models = [
+    #     '../results/20210629_20_49_43/checkpoint/model.pt',
+    #     '../results/20210629_21_00_11/checkpoint/model.pt',
+    #     '../results/20210629_21_55_37/checkpoint/model.pt',
+    # ]
+    # for m in models:
+    #     infer_image(
+    #         conf=configs3D,
+    #         image_path='/home/heyz/code/z-vision/images/test_lr'
+    #                    '/thy1_zone3_new_center_cropped_64_64_8.tif',
+    #         model_path=m,
+    #         model='new'
+    #     )
+
+    image_dir = '/home/heyz/code/z-vision/images/yj_cx3'
+    images = os.listdir(image_dir)
+
+    for img in images:
+        img_path = os.path.join(image_dir, img)
         infer_image(
             conf=configs3D,
-            image_path='/home/heyz/code/z-vision/images/test_lr'
-                       '/thy1_zone3_new_center_cropped_96_96_13.tif',
-            model_path=m,
+            image_path=img_path,
+            model_path='/home/heyz/code/z-vision/results/20210705_19_17_09/checkpoint/model.pt',
             model='new'
         )
